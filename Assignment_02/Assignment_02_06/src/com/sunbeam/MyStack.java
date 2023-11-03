d960e6dd715a41c6c0d8685fa90fd10de233dc8f
74081akash <akashkakad8059@gmail.com>
2023-11-03 15:30:26 +0530

    Assignment_02

---
+++ Assignment_02/Assignment_02_06/src/com/sunbeam/MyStack.java
@@ -0,0 +1,33 @@
+package com.sunbeam;
+
+public class MyStack 
+{
+	private int arr[];
+	private int top;
+	private final int SIZE;
+
+	public MyStack(int size) {
+		SIZE = size;
+		arr = new int[size];
+		top = SIZE;
+
+	}
+	public void push(int data) {
+		top--;
+		arr[top]=data;
+	}
+	public void pop() {
+		top++;
+	
+	}
+	public int  peek() {
+		return arr[top];
+	
+	}
+	public boolean isFull() {
+		return top==0;
+	}
+	public  boolean isEmpty() {
+		return top==SIZE;
+	}
+}
